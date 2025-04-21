a3225a659791d78fb895c16cb68ce56392b1044f
DanielCamargo1007 <jhonacamargo_10@hotmail.com>
2025-04-21 15:58:29 -0500

    Proyectos
    Se ajustan descripciones para los proyectos realizados.

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const nav = document.getElementById('nav');
 toggleButton.addEventListener('click', () => {
   nav.classList.toggle('show');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const proyectos = document.querySelectorAll('.proyecto');
+    const modales = document.querySelectorAll('.modal');
+    const botonesCerrar = document.querySelectorAll('.cerrar-modal');
+
+    proyectos.forEach(proyecto => {
+        proyecto.addEventListener('click', function() {
+            const modalId = this.dataset.modal;
+            const modal = document.getElementById(modalId);
+            modal.style.display = "block";
+        });
+    });
+
+    botonesCerrar.forEach(boton => {
+        boton.addEventListener('click', function() {
+            const modal = this.parentNode.parentNode; // Subimos dos niveles para obtener la modal
+            modal.style.display = "none";
+        });
+    });
+
+    window.addEventListener('click', function(event) {
+        modales.forEach(modal => {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        });
+    });
+});
