18981e8efd915d15873c00708bfc941cfa99cce3
DanielCamargo1007 <jhonacamargo_10@hotmail.com>
2025-04-21 09:44:25 -0500

    Menú Hamburguesa
    Se implementa el menú tipo hamburguesa para visualizar en dispositivos móviles

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Aquí puedes agregar funcionalidades más adelante
 console.log("Portafolio cargado correctamente");
+
+const toggleButton = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
+
+toggleButton.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
